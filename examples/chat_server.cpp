2a67e7f038bb6fb6cc06cab691fad5d46eee63de
Pro1d <topAffaire_pg@hotmail.fr>
2017-09-21 19:19:24 +0200

    Chat_server fix

--- examples/chat_server.cpp
+++ examples/chat_server.cpp
@@ -16,11 +16,13 @@ public:
   }
   void onConnected(unsigned int id)
   {
+    cout << "Client has joined!" << endl;
     clientId = id;
     clientConnected = true;
   }
   void onDisconnected(unsigned int id)
   {
+    cout << "Client has left!" << endl;
     if (id == clientId)
       clientConnected = false;
   }
@@ -45,17 +47,18 @@ int main(int argc, char** argv)
 {
   initServer();
   string line;
-  while (cin >> line) {
+  while (getline(std::cin, line)) {
     if (line.size() && cc->clientConnected) {
       RawBuffer buf(line.size() + 1);
       strcpy((char*)buf.getData(), line.c_str());
+      ((char*)buf.getData())[line.size()] = '\0';
       srv->send(buf, cc->clientId);
     }
     if (cc->clientConnected) {
       RawBuffer buf;
       if (srv->recv(buf, cc->clientId)) {
         const char* s = (const char*)buf.getData();
-        cout << "\e[33m" << s << "\e[0m" << endl;
+        cout << "\e[33m\"" << s << "\"\e[0m" << endl;
       }
     }
   }
