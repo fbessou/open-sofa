d4ea8048e4987e587c12b583e140002fd7df2839
Knarfux <fbessou@free.fr>
2017-10-29 19:53:58 +0100

    Add a pong example (#2)
    * Add pong
    * Add example remote control for pong example

---
+++ examples/pong.cpp
@@ -0,0 +1,84 @@
+#include "BufferedConnection.hpp"
+#include "TCPServer.hpp"
+#include <algorithm>
+#include <cstdio>
+#include <unistd.h>
+using namespace std;
+using namespace OpenSofa;
+
+class CallbackConnection : public Server::ConnectionListener {
+public:
+  CallbackConnection(Server* server) : server(server)
+  {
+  }
+  void onConnected(unsigned int id)
+  {
+    printf("\a\n");
+    cnx.push_back(new BufferedConnection(*server->getConnections()[id]));
+  }
+  void onDisconnected(unsigned int id)
+  {
+  }
+  Server* server;
+  vector<Connection*> cnx;
+};
+int main(int argc, char** argv)
+{
+  TCPServer srv(atoi(argv[1]));
+  srv.start();
+  std::shared_ptr<CallbackConnection> cc(new CallbackConnection(&srv));
+  srv.setConnectionListener(cc);
+  bool ended = false;
+  const int pw = 5;
+  int h = 20, w = 50;
+  int p1 = h / 2, p2 = h / 2;
+  int bx = 1, by = h / 2;
+  int bvx = 1, bvy = 1;
+
+  while (!ended) {
+    volatile int up1 = 0, down1 = 0, up2 = 0, down2 = 0;
+    system("clear");
+    bool a = true;
+    while (a) {
+      a = false;
+      for (auto& c : cc->cnx) {
+        uint8_t buf[2];
+        int s = c->getInputStream().read(buf, 2);
+        if (!s)
+          continue;
+        *(&up1 + buf[0] + buf[1] * 2) = 1;
+        *(&up1 + (1 - buf[0]) + buf[1] * 2) = 0;
+        a = true;
+      }
+    }
+    p1 = min(max(pw / 2 + 1, p1 - up1 + down1), h - 1 - pw / 2);
+    p2 = min(max(pw / 2 + 1, p2 - up2 + down2), h - 1 - pw / 2);
+    bx += bvx;
+    by += bvy;
+    if (by == -1)
+      by = bvy = 1;
+    else if (by == h)
+      by = h + 2 * (bvy = -1);
+    if (bx == 0)
+      bvx = bx = 1;
+    else if (bx == w - 1)
+      bx = w + 2 * (bvx = -1);
+
+    for (int y = 0; y < h; y++) {
+      for (int x = 0; x < w; x++) {
+        if (x == 0 && abs(y - p1) <= pw / 2)
+          printf("\e[31;1m|\e[0m");
+        else if (x == w - 1 && abs(y - p2) <= pw / 2)
+          printf("\e[32;1m|\e[0m");
+        else if (x == bx && y == by)
+          printf("\e[33;1m@\e[0m");
+        else
+          printf("\e[37;2m+\e[0m");
+      }
+      printf("\n");
+    }
+    usleep(1000 * 100 * 1);
+  }
+
+  return 0;
+}
---
+++ examples/remote_control.cpp
@@ -0,0 +1,108 @@
+#include <fcntl.h>
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <termios.h>
+#include <unistd.h>
+
+#include "BufferedConnection.hpp"
+#include "TCPClient.hpp"
+
+using namespace OpenSofa;
+
+enum class Key {
+  UP,
+  DOWN,
+  UNKNOWN,
+};
+
+int getInput()
+{
+  fd_set set;
+  struct timeval tv;
+
+  FD_ZERO(&set);
+  FD_SET(fileno(stdin), &set);
+
+  int res = select(fileno(stdin) + 1, &set, NULL, NULL, NULL);
+
+  if (res > 0) {
+    char c;
+    read(fileno(stdin), &c, 1);
+    return c;
+  } else if (res < 0) {
+    perror("select error");
+  } else {
+    printf("Select timeout\n");
+  }
+}
+
+Key getKey()
+{
+  char c = getInput();
+  switch (c) {
+  case 'z':
+    return Key::UP;
+  case 's':
+    return Key::DOWN;
+  default:
+    printf("no input");
+    return Key::UNKNOWN;
+  }
+}
+
+std::tuple<std::string, unsigned short> getRemoteService(int argc, char** argv)
+{
+  if (argc > 2) {
+    return std::make_pair<>(argv[1], atoi(argv[2]));
+  } else {
+    std::string hostname;
+    unsigned short port;
+    std::cout << "Server hostname/IP" << std::endl;
+    std::cin >> hostname;
+    std::cout << "Port" << std::endl;
+    std::cin >> port;
+    std::make_pair(hostname, port);
+  }
+}
+
+std::unique_ptr<Client> client;
+
+int main(int argc, char** argv)
+{
+  struct termios oldSettings, newSettings;
+
+  tcgetattr(fileno(stdin), &oldSettings);
+  newSettings = oldSettings;
+  newSettings.c_lflag &= (~ICANON & ~ECHO);
+  tcsetattr(fileno(stdin), TCSANOW, &newSettings);
+  auto service = getRemoteService(argc, argv);
+  printf("Test\n");
+  client.reset(new TCPClient(std::get<0>(service), std::get<1>(service)));
+  auto cnx_nobuf = client->connect();
+  auto cnx = new BufferedConnection(*cnx_nobuf);
+  uint8_t player = atoi(argv[3]);
+  printf("Test2\n");
+  while (true) {
+    uint8_t dir;
+    uint8_t msg[2] = { dir, player };
+    switch (getKey()) {
+    case Key::UP:
+      dir = 0;
+      break;
+    case Key::DOWN:
+      dir = 1;
+      break;
+    default:
+      continue;
+    }
+    // send direction
+    printf("%d\n", dir);
+    cnx->getOutputStream().write(msg, 2);
+  }
+
+  tcsetattr(fileno(stdin), TCSANOW, &oldSettings);
+  return 0;
+}
